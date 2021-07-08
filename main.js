5a57cc9d7b68f342fd0e39f4fc1f0ce78fc6679a
ACER <mahanasty@mail.ru>
2021-07-08 23:03:29 +0300

    feat:add styles for block video and sticky header

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ document.querySelector('.burger').addEventListener('click', function(){
     document.querySelector('.burger .burger__item').classList.toggle('burger__active');
     document.querySelector('.header').classList.toggle('clearfix');
 });
+
+/*sticky header*/
+window.onscroll = function() {myFunction()};
+
+var header = document.querySelector(".header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
